ee533c599f7166d3ea986e737f205aa3cdac2195
Leia Quesada <76133327+LeiaQuesada@users.noreply.github.com>
2021-02-19 10:48:13 -0800

    for of!!!! instead of for in

--- challenges/120.js
+++ challenges/120.js
@@ -38,9 +38,43 @@
 //
 
 function groupBy(arr, logic){
-  return(
-    // replace this line with your code
-  );
+  // return an object of keys with it's value as an array
+  let resultObj = {};
+  // iterate through the array elements, to check for multiple keys
+  for (let element of arr) {
+    // apply logic arg
+    let property = logic(element);
+    if(resultObj.hasOwnProperty(property)) {
+      let value = resultObj[property];
+      value.push(element);
+    } else {
+      // need to create an empty array for next key
+      resultObj[property] = [];
+      let value = resultObj[property];
+      value.push(element);
+    }
+  }
+    // console.log("i'm confused", resultObj);
+    return resultObj;
 }
+
+pomeranians = [
+  { name: 'Osito', weight_kg: 3.5 },
+  { name: 'Maní', weight_kg: 3.9 },
+  { name: 'Bella', weight_kg: 7.2 }
+]
+groupBy(pomeranians, (p) => Math.floor(p.weight_kg))
+/*
+ {
+  '3': [
+        { name: 'Osito', weight_kg: 3.5 },
+        { name: 'Maní', weight_kg: 3.9 }
+      ],
+  '7': [{ name: 'Bella', weight_kg: 7.2 }]
+ }
+*/
+
+groupBy(['one', 'two', 'three'], (s) => s.length);
+// => { '3': ['one', 'two'], '5': ['three'] }
 
 module.exports = groupBy;
